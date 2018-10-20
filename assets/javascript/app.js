0d99caa9f71cbe84c9809f7c56bb597cd6115d8c
jnoliver88 <jnoliver88@yahoo.com>
2018-10-20 07:14:48 -0500

    I added updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,159 +1,59 @@
-$(document).ready(function(){})
 
-var board = $('#question-area');
-var numCounter = 30;
+//variables created for each question in the HTML//
 
+function check(){
+  
+var question1 = document.quiz.question1.value;
+var question2 = document.quiz.question2.value;
+var question3 = document.quiz.question3.value;
+var question4 = document.quiz.question4.value;
+var question5 = document.quiz.question5.value;
+var question6 = document.quiz.question6.value;
+var question7 = document.quiz.question7.value;
+var question8 = document.quiz.question8.value;
+var correct = 0;
 
-///////////////////////////////////////////////////////////////////////////////
-
-//CLICK EVENTS
-//START HERE JESS// AND WORK YOUR WAYDOWN TO CHANGE THE ID AND CLASS AND MAKE SURE EVERYTHING MATCHES
-///////////////////////////////////////////////////////////////////////////////
-
-$(document).on('click', '#start-over', function() {
-  game.reset();
-});
-
-$(document).on('click', '.answer-button', function() {
-  game.clicked();
-});
-
-$(document).on('click', '#start', function(p) {
-  $('#middlesection').prepend('<h2>Time Remaining: <span id="count-down">30</span> Seconds</h2>');
-  game.loadQuestion();
-});
-
-///////////////////////////////////////////////////////////////////////////////
-
-
-//Question set
-
-
-///////////////////////////////////////////////////////////////////////////////
-
-var question = [{
-    question: "Who was the first president of the United States?",
-    answers: ["George Washington", "George W. Bush", "Lyndon B. Johnson", "John F. Kennedy"],
-    correctAnswer: "George Washington",
-}, {
-    question: "The song 'Single Ladies' was sung by which pop artist?",
-    answers: ["Ariana Grande", "Britney Spears", "Beyonce", "Rhianna"],
-    correctAnswer: "Beyonce",
-}, {
-    question: "What is the largest bone in the body?",
-    answers: ["Skull", "Femur", "Spine", "Tibia"],
-    correctAnswer: "Femur",
-}, {
-    question: "Who was the first person to walk on the moon?",
-    answers: ["Alan Bean", "David Scott", "James Irwin", "Neil Armstrong"],
-    correctAnswer: "Neil Armstrong",
-}, {
-    question: "What was the name of the lead actor in the 90s tv show 'Martin'?",
-    answers: ["Cole", "Tommy", "Martin", "Ricky"],
-    correctAnswer: "Martin",
-}, {
-    question: "What year was the first iphone released?",
-    answers: ["2001", "2007", "2005", "2002",],
-    correctAnswer: "2007",
-}, {
-    question: "Which football is best known for being called 'America's Team?",
-    answers: ["Cowboys", "Lions", "Steelers", "Eagles"],
-    correctAnswer: "Cowboys",
-}, {
-    question: "Which city is the statue of liberty located?",
-    answers: ["Los Angeles", "Miami", "Houston", "New York"],
-    correctAnswer: "New York",
-}];
-
-
-var game = {
-  question:question,
-  currentQuestion:0,
-  counter:numCounter,
-  correct:0,
-  incorrect:0,
-  countdown: function(){
-    game.counter--;
-    $('#count-down').html(game.counter);
-
-    if (game.counter === 0) {
-      console.log('TIME UP');
-      game.timeUp();
-    }
-  },
-  loadQuestion: function(){
-    timer = setInterval(game.countdown, 1000);
-    board.html('<h2>' + question[this.currentQuestion].question + '</h2>' );
-    for (var i = 0; i<question[this.currentQuestion].answers.length; i++){
-     board.append('<button class="answer-button"> id="start" + "questions[this.currentQuestion].answers[i]" + "questions[this.currentQuestion].answers[i]" + </button>');
-    }
-  },
-  nextQuestion: function(){
-    game.counter = numCounter;
-    $('#count-down').html(game.counter);
-    game.currentQuestion++;
-    game.loadQuestion();
-  },
-  timeUp: function (){
-    clearInterval(timer);
-    $('#count-down').html(game.counter);
-
-    board.html('<h2>Out of Time!</h2>');
-    board.append('<h3>The Correct Answer was: ' + questions[this.currentQuestion].correctAnswer);
-
-    if (game.currentQuestion === question.length - 1){
-      setTimeout(game.results, 3 * 1000);
-    } else {
-      setTimeout(game.nextQuestion, 3 * 1000);
-    }
-  },
-  results: function() {
-    clearInterval(timer);
-
-    board.html('<h2>All done, heres how you did!</h2>');
-    $('#count-down').html(game.counter);
-    board.append('<h3>Correct Answers: ' + game.correct + '</h3>');
-    board.append('<h3>Incorrect Answers: ' + game.incorrect + '</h3>');
-    board.append('<h3>Unanswered: ' + (question.length - (game.incorrect + game.correct)) + '</h3>');
-    board.append('<br><button id="start-over">Start Over?</button>');
-  },
-  clicked: function(e) {
-    clearInterval(timer);
-
-    if ($(e.target).data("name") === question[this.currentQuestion].correctAnswer){
-      this.answeredCorrectly();
-    } else {
-      this.answeredIncorrectly();
-    }
-  },
-  answeredIncorrectly: function() {
-    game.incorrect++;
-    clearInterval(timer);
-    board.html('<h2>Nope!</h2>');
-    board.append('<h3>The Correct Answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
-
-    if (game.currentQuestion === question.length - 1){
-      setTimeout(game.results, 3 * 1000);
-    } else {
-      setTimeout(game.nextQuestion, 3 * 1000);
-    }
-  },
-  answeredCorrectly: function(){
-    clearInterval(timer);
-    game.correct++;
-    board.html('<h2>Correct!</h2>');
-
-    if (game.currentQuestion === questions.length - 1){
-      setTimeout(game.results, 3 * 1000);
-    } else {
-      setTimeout(game.nextQuestion, 3 * 1000);
-    }
-  },
-  reset: function(){
-    this.currentQuestion = 0;
-    this.counter = numCounter;
-    this.correct = 0;
-    this.incorrect = 0;
-    this.loadQuestion();
+//if statements created to equal the value of the right answer. Im not that confident yet using loops inside of fucntions etc. because i get confused// 
+  if (question1 == "George Washington") {
+    correct++;
   }
-};
+  if (question2 == "Beyonce") {
+    correct++;
+  }
+  if (question3 == "Femur") {
+    correct++;
+  }
+  if (question4 == "Neil Armstrong") {
+    correct++;
+  }
+  if (question5 == "Martin") {
+    correct++;
+  }
+  if (question6 == "2007") {
+    correct++;
+  }
+  if (question7 == "Cowboys") {
+    correct++;
+  }
+  if (question8 == "New York") {
+    correct++;
+  }
+
+
+  var messages = ["Awesome, You got them all right", "C'mon, I know you can do better", "Really Bruh"]
+  var range;
+  
+  if (correct < 4){
+      range = 2;
+  }
+  if (correct > 0 && correct < 8) {
+      range = 1;
+  }
+  if (correct > 0) {
+      range = 0;
+  }
+  document.getElementById("after_submit").style.visibility = "visible";
+
+  document.getElementById("messages").innerHTML = messages[range];
+  document.getElementById("number_correct").innerHTML = "You got " + correct + "correct.";
+}
